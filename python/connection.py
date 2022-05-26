db5295235147fdee2c041cab0897963e09682f13
Richard Channing <richardsobelnkulu@gmail.com>
2022-05-26 16:00:49 +0200

    deuxieme commit

--- python/connection.py
+++ python/connection.py
@@ -1,6 +1,7 @@
 from flask import Flask 
 from flaskext.mysql import MySQL 
 mysql = MySQL() 
+
 app=Flask(__name__) 
 app.config['MYSQL_DATABASE_USER'] ="username" 
 app.config['MYSQL_DATABASE_PASSWORD'] = "password"
